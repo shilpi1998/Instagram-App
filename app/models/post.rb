351ca14ea900fb1e792008a292cce0307bad9d4e
Shilpi Mittal <56123020+shilpi1998@users.noreply.github.com>
2020-09-22 05:22:04 +0530

    added infinite scrolling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_attached_file :image
-  #validates :image, presence: true
+  validates :image, :caption, presence: true
 
   #has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
